4616865c88a457e3d1e1b2dc391c1e59073b5524
Mahesh <maheshkalluri686@gmail.com>
2021-08-27 22:07:59 +0200

    Log4J

---
+++ src/varun/Memo.java
@@ -0,0 +1,41 @@
+package varun;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.*;
+
+
+
+public class Memo {
+
+		private static Logger log1 =(Logger) LogManager.getLogger(Memo.class.getName());
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		log1.debug("I am debug");
+		log1.error("I am error");
+		log1.info("I am info");
+		log1.fatal("I am fatal");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
